e254cba20d387401cb37defc94fc9e562dfad890
Petri <petri.karhunkoski@gmail.com>
2019-11-25 16:26:14 +0200

    fixes Assert.* import, which failed checkstyle

--- src/test/java/jotainhauskaa/vinkkilista/domain/KirjaVinkkiTest.java
+++ src/test/java/jotainhauskaa/vinkkilista/domain/KirjaVinkkiTest.java
@@ -3,7 +3,8 @@ package jotainhauskaa.vinkkilista.domain;
 
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 
 public class KirjaVinkkiTest {
