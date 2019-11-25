8af2209c65d31e51473b85b618ea7b6220eae417
Petri <petri.karhunkoski@gmail.com>
2019-11-25 14:18:40 +0200

    checkstyle fixes

--- src/main/java/jotainhauskaa/vinkkilista/controllers/TipController.java
+++ src/main/java/jotainhauskaa/vinkkilista/controllers/TipController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jotainhauskaa.vinkkilista.domain.KirjaVinkki;
@@ -13,8 +13,9 @@ import jotainhauskaa.vinkkilista.dao.MuistiKirjaVinkkiDao;
 
 @Controller
 public class TipController {
-    KirjaVinkki kirjavinkki;
-    MuistiKirjaVinkkiDao muistiKirjaVinkkiDao = new MuistiKirjaVinkkiDao ();
+    private KirjaVinkki kirjavinkki;
+    private MuistiKirjaVinkkiDao muistiKirjaVinkkiDao 
+            = new MuistiKirjaVinkkiDao();
 
     /*
      * @RequestMapping("/")
@@ -32,18 +33,22 @@ public class TipController {
     }
 
     @PostMapping("/lisaavinkki")
-    public String vinkinLisays(@RequestParam String kirjoittaja, @RequestParam String otsikko, 
-    @RequestParam String tyyppi, @RequestParam String isbn, @RequestParam String kuvaus, 
-    @RequestParam String kommentti, @RequestParam String tagit, @RequestParam String kurssit) {
-        //tällä hetkellä tageja ja kursseja on vain yhden. jatkossa tehdään tämä jollakin muulla tavalla
-        String t [] = new String[1];
+    public String vinkinLisays(@RequestParam String kirjoittaja,
+    @RequestParam String otsikko, @RequestParam String tyyppi,
+    @RequestParam String isbn, @RequestParam String kuvaus,
+    @RequestParam String kommentti, @RequestParam String tagit,
+    @RequestParam String kurssit) {
+        // tällä hetkellä tageja ja kursseja on vain yhden. 
+        // jatkossa tehdään tämä jollakin muulla tavalla
+        String[] t = new String[1];
         t[0] = tagit;
-        String k [] = new String[1];
+        String[] k = new String[1];
         k[0] = kurssit;
-        muistiKirjaVinkkiDao.add(new KirjaVinkki(kirjoittaja, otsikko, tyyppi, isbn, kuvaus, kommentti, t, k));
+        muistiKirjaVinkkiDao.add(new KirjaVinkki(kirjoittaja, otsikko, tyyppi, 
+                                                isbn, kuvaus, kommentti, t, k));
         return "redirect:/";
-
     }
+
     @GetMapping("/selaa")
     public String vinkkienSelailu(Model model) {
         model.addAttribute("vinkit", muistiKirjaVinkkiDao.getAll());
