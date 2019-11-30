3246d6645c581d5e95bc75ac764184d65279ea0f
juusotaneli <juuso.karvonen@outlook.com>
2019-11-30 13:56:06 +0200

    fixes

--- src/main/java/jotainhauskaa/vinkkilista/domain/Vinkki.java
+++ src/main/java/jotainhauskaa/vinkkilista/domain/Vinkki.java
@@ -1,7 +1,6 @@
 package jotainhauskaa.vinkkilista.domain;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
