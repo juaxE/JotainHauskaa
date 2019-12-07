77b2bcb16a5736d9ab6b1e6ce989457d4e8e8a4f
k0tix <otto.pirttimaa@gmail.com>
2019-12-07 19:24:59 +0200

    Add initial controller tests

---
+++ src/test/java/jotainhauskaa/vinkkilista/controllers/TipControllerTest.java
@@ -0,0 +1,101 @@
+package jotainhauskaa.vinkkilista.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import jotainhauskaa.vinkkilista.dao.VinkkiRepository;
+import jotainhauskaa.vinkkilista.domain.KirjaVinkki;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class TipControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private VinkkiRepository vinkit;
+
+    @Before
+    public void setUp() {
+        KirjaVinkki vinkki = new KirjaVinkki();
+        vinkki.setOtsikko("Testi otsikko");
+        vinkit.save(vinkki);
+    }
+
+    @Test
+    public void sovellusPalauttaaEtusivun() throws Exception {
+        MvcResult res = mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+        assertTrue(content.contains("Tervetuloa JotainHauskaa"));
+    }
+
+    @Test
+    public void sovellusPalauttaaLisaysFormin() throws Exception {
+        MvcResult res = mockMvc.perform(get("/lisaavinkki"))
+            .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+        assertTrue(content.contains("Lisää kirjavinkki"));
+    }
+
+    @Test
+    public void sovellusPalauttaaSelausSivun() throws Exception {
+        MvcResult res = mockMvc.perform(get("/selaa"))
+            .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+        assertTrue(content.contains("Lisätyt kirjavinkit"));
+    }
+
+    @Test public void lisaaFormPalauttaaVirheet() throws Exception {
+        MvcResult res = mockMvc.perform(
+            post("/lisaavinkki")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED).characterEncoding("UTF-8"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+        assertTrue(content.contains("Otsikko ei voi olla tyhjä"));
+    }
+
+    @Test public void lisaaFormMeneeLapi() throws Exception {
+        mockMvc.perform(
+            post("/lisaavinkki")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED).characterEncoding("UTF-8")
+                .param("otsikko", "otsikko"))
+            .andExpect(status().is3xxRedirection());
+    }
+
+    @Test public void sovellusPalauttaaHakuSivun() throws Exception {
+        MvcResult res = mockMvc.perform(
+            get("/hae")
+                .param("haku", "Testi"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+        assertTrue(content.contains("Hakutulokset haulla:"));
+    }
+}
