5aeb511bf51d88c2083dd8da82e9930a2b54dff2
Markus Lumme <markusjuhani.lumme@gmail.com>
2019-12-09 16:32:04 +0200

    Korjattu ISBN:n perusteella Google Booksista hakevan apuluokan toimintaa korvaamaan null-arvoiset kentät KirjaVinkin kentiin sopivilla arvoilla

--- src/main/java/jotainhauskaa/vinkkilista/util/ISBNLoader.java
+++ src/main/java/jotainhauskaa/vinkkilista/util/ISBNLoader.java
@@ -8,6 +8,7 @@ import com.google.api.services.books.Books.Volumes.List;
 import com.google.api.services.books.model.Volume;
 import com.google.api.services.books.model.Volumes;
 import com.google.api.services.books.model.Volume.VolumeInfo;
+
 import com.google.api.client.http.javanet.NetHttpTransport;
 
 public class ISBNLoader {
@@ -43,12 +44,27 @@ public class ISBNLoader {
         VolumeInfo volumeInfo = volume.getVolumeInfo();
         
         String kirjoittaja = volumeInfo.getAuthors().get(0);
+        kirjoittaja = kirjoittaja != null ? kirjoittaja : "";
+
         String otsikko = volumeInfo.getTitle();
+
         String tyyppi = "Kirja";
+
         String isbn = volumeInfo.getIndustryIdentifiers().get(0).getIdentifier();
-        String kuvaus = volumeInfo.getDescription().substring(0, 255);
+        isbn = isbn != null ? isbn : "";
+
+        String kuvaus = volumeInfo.getDescription();
+        kuvaus = kuvaus != null ? kuvaus : "";
+        if (kuvaus.length() > 255) {
+            kuvaus = kuvaus.substring(0, 252) + "...";
+        }
+
         String kommentti = "";
-        String[] tagit = volumeInfo.getCategories().toArray(new String[0]);
+
+        String[] tagit = volumeInfo.getCategories() != null 
+            ? volumeInfo.getCategories().toArray(new String[0]) 
+            : new String[] {};
+
         String[] kurssit = new String[] {};
         
         KirjaVinkki vinkki = new KirjaVinkki(kirjoittaja, otsikko, 
